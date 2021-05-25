4d6417ba55a2e55f773129c3fc03d8dc3744deaa
SergioAPineda <sergio.pinedas94@gmai.com>
2021-05-24 22:48:31 -0500

    keyboard event listener to move the bars was added

--- main.js
+++ main.js
@@ -17,6 +17,19 @@
   }
 })();
 
+(function(){
+  self.Ball = function(x,y,radius, board){
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.speed_y = 0;
+    this.speed_x = 3;
+    this.board = board;
+    board.ball = this;
+    this.kind = "circle";
+  }
+})();
+
 (function () {
   self.BoardView = function (canvas, board) {
     this.canvas = canvas;
@@ -27,24 +40,34 @@
   }
 
   self.BoardView.prototype = {
+    clean: function(){
+      this.ctx.clearRect(0,0, board.width, board.height);
+    },
     draw: function () {
       for (var i = this.board.elements.length - 1; i >= 0; i--) {
         var el = this.board.elements[i];
         draw(this.ctx, el);
       }
+    },
+    play: function(){
+      this.clean()
+      this.draw();
     }
   }
 
   function draw(ctx, element) {
-    if (element !== null && element.hasOwnProperty("kind")) {
-      switch (element.kind) {
-        case "rectangle":
-          ctx.fillRect(element.x, element.y, element.width, element.height);
-          break;
-      }
+    switch (element.kind) {
+      case "rectangle":
+        ctx.fillRect(element.x, element.y, element.width, element.height);
+        break;
+      case "circle":
+        ctx.beginPath();
+        ctx.arc(element.x, element.y, element.radius,0,7);
+        ctx.fill();
+        ctx.closePath();
+        break;
     }
   }
-
 })();
 
 (function () {
@@ -56,7 +79,7 @@
     this.board = board;
     this.board.bars.push(this);
     this.kind = "rectangle";
-    this.speed = 10;
+    this.speed = 15;
   }
 
   self.Bar.prototype = {
@@ -77,18 +100,27 @@ let bar = new Bar(20, 100, 40, 100, board);
 let bar2 = new Bar(730, 100, 40, 100, board);
 var canvas = document.getElementById("canvas");
 var board_View = new BoardView(canvas, board);
+let ball = new Ball(350,100,10,board); 
+
+window.requestAnimationFrame(controller);
 
 document.addEventListener("keydown", function (ev) {
+  ev.preventDefault();
   if (ev.key == 'ArrowUp') {
     bar.up();
   } else if (ev.key == 'ArrowDown') {
     bar.down();
   }
-  console.log(""+bar);
+  if (ev.key == 'ArrowRight') {
+    bar2.up();
+  } else if (ev.key == 'ArrowLeft') {
+    bar2.down();
+  }
 });
 
-self.addEventListener("load", main);
+self.addEventListener("load", controller);
 
-function main() {
-  board_View.draw();
+function controller() {
+  board_View.play();
+  window.requestAnimationFrame(controller);
 };
