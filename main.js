96343b356c3e851196d7098c5c24c0f17c84bf03
SergioAPineda <sergio.pinedas94@gmai.com>
2021-05-24 19:18:55 -0500

    Board and BoardView have been created

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-document.write("Hola Mundo");
+(function(){
+  self.Board = function(width, height){
+    this.width = width;
+    this.height = height;
+    this.playing = false;
+    this.game_over = false;
+    this.bars = [];
+    this.ball = null;
+  }
+  
+  self.Board.prototype = {
+    get elements(){
+      var elements = this.bars;
+      elements.push(ball);
+      return elements;
+    }
+  } 
+})();
+
+(function(){
+  self.BoardView = function(canvas,board) {
+    this.canvas = canvas;
+    this.canvas.width = board.width;
+    this.canvas.height = board.height;
+    this.board = board
+    this.ctx = canvas.getContext("2d");
+  }
+})();
+
+window.addEventListener("load", main);
+
+function main() {
+  var board = new Board(800, 400);
+  var canvas = document.getElementById("canvas");
+  var board_View = new BoardView(canvas, board);
+}
